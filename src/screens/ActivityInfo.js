541425355447a798aa4d4ab149583808339aace2
Tushar <tusharrahuljain@gmail.com>
2022-03-13 02:22:57 +0530

    Events Details Provided

--- src/screens/ActivityInfo.js
+++ src/screens/ActivityInfo.js
@@ -1,18 +1,26 @@
 import React,{useState,useEffect} from 'react';
 import {View,Text,FlatList} from 'react-native';
 import {BASE_URL,token} from '../utils/api.js';
-import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-cards';
+import { WebView } from 'react-native-webview';
 
 function ActivityInfo({navigation,route}){
 const {act}=route.params;
 const [loading,setLoading]=useState(true);
 
     return(
-        <View>
-            
-            <Text>{act.name}</Text>
+        <View style={{borderColor:'black',borderWidth:1,margin:7,paddingHorizontal:5,paddingBottom:40,borderRadius:7,backgroundColor:'#FAE984'}}>
+        <View style={{height:250}}>
+            <WebView
+            source={{ uri: act.video_link}}
+            style={{ height:200,margin:10}}
+            onError={(event)=> alert('Webview error'+event.nativeEvent.description)}
+            />
+        </View>
+        <View style={{margin:5}}>
+            <Text style={{fontSize:18,fontWeight:'bold',color:'#FA8546'}}>{act.name}</Text>
             <Text>{act.description}</Text>
         </View>
+    </View>
     )
 }
 
--- src/screens/EventInfo.js
+++ src/screens/EventInfo.js
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from "react";
-import {View,Text,StyleSheet,FlatList} from "react-native";
+import {View,Text,StyleSheet,FlatList,ActivityIndicator,Image} from "react-native";
 import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-cards';
-import {BASE_URL} from '../utils/api.js'
+import {BASE_URL,token} from '../utils/api.js'
 function EventInfo(){
     const [events,setEvents]=useState([]);
     const [loading,setLoading]=useState(true);
@@ -9,13 +9,13 @@ function EventInfo(){
     const getEvents=async()=>{
         setLoading(true);
         try{
-            const result=await fetch(BASE_URL+'/Activity/',{
+            const result=await fetch(BASE_URL+'/Event/',{
                 method:'GET',
                 headers: {'Authorization': 'token '+token},
             });
             const json= await result.json();
             console.log(json);
-            setData(json);
+            setEvents(json);
         }catch(error){
             console.log(error);
             // Alert.alert(error);
@@ -26,14 +26,40 @@ function EventInfo(){
 
     useEffect(() => {
         // console.log(BASE_URL);
-        // getData();
+        getEvents();
       }, []);
 
     return(
-        <View>
-            <Text>Events</Text>
-        </View>
+        <>{loading?<ActivityIndicator/>:
+        <FlatList
+              data={events}
+              keyExtractor={({ id }, index) => id}
+              renderItem={({item,index})=>
+              <View style={styles.card}>
+                  {item.image?<Image source={{uri:item.image}} style={{height:200,marginTop:5}}/>:
+                  <Image source={require('../utils/undraw_game_day_ucx9.png')} style={{height:200,width:400,marginTop:5,}}/>
+                  }
+                  <Text style={{fontSize:18,fontWeight:'bold',color:'#FA8546'}}>{item.name}</Text>
+                  <View style={{flexDirection:'row',justifyContent:'space-between',marginHorizontal:5}}>
+                    <Text>Date: {item.date}</Text>
+                    <Text>Participant Limit: {item.participant_limit}</Text>
+                  </View>
+                    <Text>Location: {item.location}</Text>
+              </View>
+            }
+            />}
+        </>
     )
 }
+
+const styles=StyleSheet.create({
+    card:{
+        borderColor:'black',
+        borderWidth:1,
+        margin:7,
+        paddingHorizontal:5,paddingBottom:40,
+        borderRadius:7,backgroundColor:'#FAE984'
+        }
+})
 
 export default EventInfo;
